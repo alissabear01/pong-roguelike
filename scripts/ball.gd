e319936e4cb6a55c8fcb0f5bd6f16cf01fde353d
alissabear01 <84651547+alissabear01@users.noreply.github.com>
2026-09-14 18:38:18 -0700

    2 | Setup Basic Level Room
    Add ball, tileset walls, background
    https://trello.com/c/xqqdpymq/2-setup-room-for-basic-level

---
+++ scripts/ball.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+## The ball, moves at a constant speed & reflects off any surface it hits
+## uses CharacterBody2D rather than RigidBody2D so we control the exact velocity vector
+
+## ball speed in pixels per s, can tweak in inspector
+@export var speed: float = 300.0
+
+# runs when ball enters the scene
+func _ready() -> void:
+	# picks random launch angle between 45 & 135 degrees so ball goes downwardish & adds velocity -- can change later
+	var angle := randf_range(PI * 0.25, PI * 0.75)
+	velocity = Vector2.RIGHT.rotated(angle) * speed
+
+# runs 60x per s on physics clock. movement is here
+func _physics_process(delta: float) -> void:
+	# try to move. if hit something, return collision info null if not. delta converts px per s to px this frame
+	var collision := move_and_collide(velocity * delta)
+	if collision:
+		# get_normal() is direction the suface faces. bounce() reflects our velocity off it
+		velocity = velocity.bounce(collision.get_normal())
